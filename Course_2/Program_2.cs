de7b4b420ea96c4609b4c3d6eca0450cd5799a72
Roberto Vieira Carlos <beto.vieiracarlos@gmail.com>
2021-10-07 00:09:25 -0300

    Novas anotações

---
+++ Course_2/Program_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Course_2
+{
+    class Program_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entendendo o SPLIT-------------------------------------------------------");
+            Console.WriteLine();
+
+            string s = Console.ReadLine();
+
+            string[] vet = s.Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+        }
+    }
+}
+ 
