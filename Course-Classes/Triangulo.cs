f94fe64a152a65ff45b7c8e0898b449af4093d07
Roberto Vieira Carlos <79486045+beto-desenv@users.noreply.github.com>
2021-11-22 12:32:52 -0300

    Classes

---
+++ Course-Classes/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Classes
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C)); // Função responsavel pelo calculo da raiz quadrada de "p".
+        }
+
+    }
+}
