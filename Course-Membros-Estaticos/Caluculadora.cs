58089fce954ca62be526f99b1aa387563b2763f1
Roberto Vieira Carlos <79486045+beto-desenv@users.noreply.github.com>
2021-12-05 23:32:49 -0300

    Metodo-Estatico

---
+++ Course-Membros-Estaticos/Caluculadora.cs
@@ -0,0 +1,16 @@
+﻿namespace Course_Membros_Estaticos
+{
+    class Caluculadora
+    {
+        public static double Pi = 3.14;
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
---
+++ Course-Membros-Estaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_Membros_Estaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Caluculadora.Circunferencia(raio);
+            double volume = Caluculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Caluculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
