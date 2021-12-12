787768ad1390b54dab82001cebaa168ce669c760
Roberto Vieira Carlos <79486045+beto-desenv@users.noreply.github.com>
2021-12-12 19:11:08 -0300

    Encapsulamento

---
+++ Course-Encapsulamento-Auto-Properties/Produto.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+
+namespace Course_Encapsulamento_Auto_Properties
+{
+    class Produto
+    {
+        private string _nome; // <== Nesse caso, não é possivel usar o AUTO PROPERTIES devido a Lógica aplicada à Propriedade.
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set // Lógica de negócio, não permite que o valor seja VAZIO e que seja somente MAIOR que "1" 
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) // Método resposável por adiocionar Produtos.
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) // Método resposável por remover Produtos.
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course-Encapsulamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+        }
+    }
+}
+
+
+
+
