4e0d41b0e96065bd0c6903f0fd3bfa87b803314b
Roberto Vieira Carlo - 37617458851 <roberto.carlo@animaeducacao.com.br>
2022-01-03 22:10:48 -0300

    Vetores

---
+++ Course-Vetores/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Course_Vetores
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Course-Vetores/Program-Parte 1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+
+/// - Revisão do conceito de vetor;
+/// - Manipulação de vetor de elementos tipo structs;
+/// - Manipulação de vetor de elementos tipo classe;
+/// - Acesso aos elementos;
+/// - Propriedade Length.
+
+//namespace Course_Vetores
+//{
+//    class Program_Parte_1
+//    {
+//        //static void Main(string[] args)
+//        //{
+//        //    int n = int.Parse(Console.ReadLine());
+
+//        //    double[] vect = new double[n];
+
+//        //    for (int i = 0; i < n; i++)
+//        //    {
+//        //        vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//        //    }
+
+//        //    double sum = 0.0;
+//        //    for (int i = 0; i < n; i++)
+//        //    {
+//        //        sum += vect[i];
+//        //    }
+
+//        //    double avg = sum / n;
+
+//        //    Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+//        //}
+//    }
+//}
